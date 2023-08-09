485cc19ce5e2bd04a291b503e4fdddd829aa7585
ArikRunyon <arikrunyon713@hotmail.com>
2023-08-09 17:22:07 -0400

    cut lines

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -19,51 +19,66 @@ export class GildedRose {
 
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
+      // ------------- REFACTORED CODE ---------------------
+      let item = this.items[i]
+
+      if (!/Brie|Backstage|Sulfuras/.test(item.name) && item.quality > 0) {
+        item.quality -= 1
+      }
+      else {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1
+          if (item.name.includes("Backstage") && item.quality < 50 && item.sellIn < 11) {
+            item.sellIn < 6 ? item.quality += 2 : item.quality += 1
           }
         }
       }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        }
+      
+      if (!item.name.includes("Sulfuras")) { item.sellIn -= 1; }
+      
+      if (item.sellIn < 0) {
+        if (item.name == "Aged Brie" && item.quality < 50) {item.quality += 1}
+        if (item.name.includes("Backstage")) {item.quality = 0}
+        if (!/Brie|Backstage|Sulfuras/.test(item.name) && item.quality != 0) {item.quality -= 1}
       }
     }
-
     return this.items;
   }
 }
+
+// ------------- ORIGINAL CODE ---------------------
+
+// if (item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+  //   if (item.quality > 0) {
+    //     if (item.name != 'Sulfuras, Hand of Ragnaros') {
+      //       item.quality = item.quality - 1
+      //     }
+      //   }
+      // } 
+      // if (item.sellIn < 11) {
+        // if (item.quality < 50) {
+          // item.quality = item.quality + 1
+          // }
+          // }
+      // if (item.sellIn < 6) {
+        // if (item.quality < 50) {
+          // item.quality += 2
+          // }
+          // } else {
+        // item.quality += 1
+        // }
+        // if (item.name != 'Aged Brie') {//
+        //   if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {//
+        //     if (item.quality > 0) {
+        //       if (item.name != 'Sulfuras, Hand of Ragnaros') {
+        //         item.quality = item.quality - 1
+        //       }
+        //     }
+        //   } else {//
+        //     item.quality = item.quality - item.quality//
+        //   }//
+        // } else {//
+        //   if (item.quality < 50) {//
+        //     item.quality = item.quality + 1//
+        //   }//
+        // }//
--- TypeScript/test/golden-master-text-test.ts
+++ TypeScript/test/golden-master-text-test.ts
@@ -22,9 +22,9 @@ if (process.argv.length > 2) {
 
 for (let i = 0; i < days; i++) {
   console.log("-------- day " + i + " --------");
-  console.log("name, sellIn, quality");
+  console.log("name | sellIn | quality");
   items.forEach(element => {
-    console.log(element.name + ' ' + element.sellIn + ' ' + element.quality);
+    console.log(element.name + ' | ' + element.sellIn + ' | ' + element.quality);
 
   });
   console.log();
